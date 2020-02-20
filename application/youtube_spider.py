b6efe75f40502ab1998cb2490b928f7b4680f78f
Mohammad Hossein <iammhz77@gmail.com>
2020-02-20 21:29:45 +0330

    saving links to file

--- application/youtube_spider.py
+++ application/youtube_spider.py
@@ -8,7 +8,10 @@ from scrapy.crawler import CrawlerProcess
 
 
 class YoutubeSpider(scrapy.Spider):
-    base_download_url = 'https://www.ssyoutube.com'
+    download_link_prefix = 'https://www.ssyoutube.com'
+    youtube_link_prefix = 'https://www.youtube.com'
+    file_name = 'links.txt'
+
     name = "youtube"
 
     def __init__(self, *args, **kwargs):
@@ -24,16 +27,21 @@ class YoutubeSpider(scrapy.Spider):
         links = response.css('a::attr(href)').extract()
         # removing repetitive links by converting list to set
         links = set(links)
+
         for link in links:
             if str(link).__contains__('index'):
                 # print(link)
 
-                download_page_link = YoutubeSpider.base_download_url + link
+                download_page_link = YoutubeSpider.download_link_prefix + link
+                youtube_page_link = YoutubeSpider.youtube_link_prefix + link
+
                 # open download link in default browser
                 self.open_link_in_browser(download_page_link)
 
                 print(download_page_link)
                 self.write_to_file(download_page_link)
+                self.write_to_file(youtube_page_link)
+                self.write_to_file("")
 
                 # self.crawl_inside_download_page(download_page_link)
 
@@ -57,8 +65,8 @@ class YoutubeSpider(scrapy.Spider):
             self.write_to_file(dl)
 
     def write_to_file(self, text):
-        f = open("links.txt", "a")
-        f.write(str(text.encode('utf-8')) + '\n')
+        f = open(YoutubeSpider.file_name, "a")
+        f.write(str(text) + '\n')
         f.close()
     # container > h1 > yt-formatted-string
 
@@ -68,5 +76,7 @@ def extract_links(link):
     process.crawl(YoutubeSpider, link)
     process.start()
 
-# link = 'https://www.youtube.com/watch?v=EeFqcmaw6iA&list=PL7u4lWXQ3wfKqFjzTSBmylEeVP9ibx_XT'
-# extract_links(link)
+
+if __name__ == '__main__':
+    link = 'https://www.youtube.com/watch?v=EeFqcmaw6iA&list=PL7u4lWXQ3wfKqFjzTSBmylEeVP9ibx_XT'
+    extract_links(link)
