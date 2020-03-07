611e0eea1cb952c68216fddecfc5a53443648079
Mohammad Hossein <iammhz77@gmail.com>
2020-03-07 15:39:54 +0330

    set ui title

--- application/ui_window.py
+++ application/ui_window.py
@@ -68,5 +68,5 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Link Extractor"))
         self.extractButton.setText(_translate("MainWindow", "Extract"))
